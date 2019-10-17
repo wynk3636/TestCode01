47dbc9ed86ee3259153b9115c869cf8a1b557ee3
ishida <h_hand_3636@gmail.com>
2019-10-17 23:14:20 +1100

    First Commit

---
+++ src/main/java/test01/Calculator.java
@@ -0,0 +1,7 @@
+package test01;
+
+public class Calculator {
+	public static int add (int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/test01/CalculatorTest.java
@@ -0,0 +1,17 @@
+package test01;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+
+	@Test
+	public void testAdd() {
+		int actual = Calculator.add(1,2);
+		assertThat(actual, is(3));
+	}
+	
+
+}
